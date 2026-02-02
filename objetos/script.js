222520a95be500908ccfe3f9b5ef92f1be62f536
EX-Stras <stras3pp@gmail.com>
2026-02-02 12:59:32 -0300

    nada

--- objetos/script.js
+++ objetos/script.js
@@ -1,4 +1,4 @@
-var Joao = {
+/*var Joao = {
     idade: 40,
     profissao: 'programador',
 }
@@ -18,3 +18,40 @@ var triangulo = {
 console.log(triangulo.perimetro(5, 10, 16))
 
 console.log(Math.random())
+
+var section = {
+    width: 800,
+    height: 50,
+    border: 1,
+    color: '#84e',
+    backgroundColor: '#fff',
+    metadeHeight(){
+        return this.height / 2;
+    }
+}
+
+section.padding = 10;
+section.backgroundColor = '#ff0';
+*/
+
+
+
+//////////////////////////////////////////////////////
+
+
+
+// Crie um objeto com os seus dados pessoais
+// Deve possui pelo menos duas propriedades nome e sobrenome
+
+// Crie um método no objeto anterior, que mostre o seu nome completo
+
+// Modifique o valor da propriedade preco para 3000
+var carro = {
+  preco: 1000,
+  portas: 4,
+  marca: 'Audi',
+}
+
+// Crie um objeto de um cachorro que represente um labrador,
+// preto com 10 anos, que late ao ver um homem
+
