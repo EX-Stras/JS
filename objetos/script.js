bd9846c4721ecf70b49d192484d4903a659aa512
EX-Stras <stras3pp@gmail.com>
2026-02-02 22:59:47 -0300

    nada

--- objetos/script.js
+++ objetos/script.js
@@ -43,7 +43,17 @@ section.backgroundColor = '#ff0';
 // Crie um objeto com os seus dados pessoais
 // Deve possui pelo menos duas propriedades nome e sobrenome
 
+var eu = {
+    nome: 'Lucas',
+    sobrenome: 'Menêzes Cândido',
+    idade: 14,
+}
+
 // Crie um método no objeto anterior, que mostre o seu nome completo
+
+    eu.nomeCompleto = function(){
+        return `${this.nome} ${this.sobrenome}`
+    }
 
 // Modifique o valor da propriedade preco para 3000
 var carro = {
@@ -52,6 +62,21 @@ var carro = {
   marca: 'Audi',
 }
 
+carro.preco = 3000;
+
 // Crie um objeto de um cachorro que represente um labrador,
 // preto com 10 anos, que late ao ver um homem
 
+var cachorro = {
+    raça: 'labrador',
+    idade: 10,
+    cor: 'preto',
+
+    latir(pessoa){
+        if (pessoa === 'homem'){
+            return 'AU AU AU AU!!!'
+        } else {
+            return 'ZZZZZZZZ...'
+        }
+    }
+}
---
+++ tudoE_objeto/script.js
@@ -0,0 +1,48 @@
+/*var n = 'Lucas',
+    nomezinho = n.toLowerCase();
+    console.log(nomezinho)
+
+console.log(n.length)
+console.log(n.charAt(0), n.charAt(1))
+var nominho = n.replace('cas','quinhas')
+console.log(nominho)
+
+var num = 2.8;
+
+var testes = {
+    funcao(a, oi){
+        return 'ovo';
+    },
+}
+
+var btn = document.querySelector('.btn')
+
+btn.addEventListener('click', function(){
+
+})*/
+
+
+
+//////////////////////////////////////////////////////
+
+
+// nomeie 3 propriedades ou métodos de strings
+var oi = 'oi';
+ //length
+ //charAt
+ //replace
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+
+var btn = document.querySelector('.btn')
+
+//addEventListener
+//after
+//classList
+//scroll
+//download
+
+// busque na web um objeto (método) capaz de interagir com o clipboard, 
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
+
+///////////clipboardData///////////
