7b474d6cfc036b7df1acb44249184a8cbb9079a3
EX-Stras <stras3pp@gmail.com>
2026-02-05 11:19:43 -0300

    nada

--- Cap3/DOM/script.js
+++ Cap3/DOM/script.js
@@ -3,6 +3,40 @@ console.log(window);
 console.log(window.innerHeight, window.innerWidth)
 
 if (window.innerWidth > 700)
- console.log(window.alert('Olá'))
+    console.log(window.alert('Olá'))
 
 titulo = document.querySelector('main');
+console.log(titulo.classList)
+console.log(titulo.innerText)
+console.log(titulo.id)
+console.log(titulo.offsetWidth)
+console.log(titulo.offsetHeight)
+
+titulo.addEventListener('click', function(){
+console.log('Clicou em', titulo)
+})
+
+console.log('///////////////////////////////')
+
+// Retorne o url da página atual utilizando o objeto window
+
+console.log(window.location.href)
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+
+ativo = document.querySelector('.ativo');
+
+console.log(ativo)
+
+listas = document.querySelectorAll('#lista')
+
+console.log(listas)
+// Retorne a linguagem do navegador
+
+
+console.log(window.navigator.language)
+
+// Retorne a largura da janela 
+
+console.log(window.innerWidth)
