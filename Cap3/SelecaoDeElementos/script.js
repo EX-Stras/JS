9e727316e0a81704aeb0540e0eabbaa6375122b7
EX-Stras <stras3pp@gmail.com>
2026-02-17 00:24:40 -0300

    nada

--- Cap3/SelecaoDeElementos/script.js
+++ Cap3/SelecaoDeElementos/script.js
@@ -15,4 +15,20 @@ const query1 = document.querySelector('.faq');
 const query2 = query1.querySelector('p:last-child');
 
 console.log(query1.innerHTML)
+
 console.log(query2.innerText)
+
+const tio1 = document.querySelector('dl');
+
+const tio2 = tio1.getElementsByClassName('title3')
+
+for (let x = 0; x < 120; x++){
+console.log(tio2[x].innerText)  
+
+if (tio2[x + 1] === undefined)
+    x = 121;
+}
+
+const linksInternos = document.querySelectorAll('[href^="#"]')
+
+console.log(linksInternos)
