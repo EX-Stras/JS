13cd2e712932be0b58545ef666d3ae8bd993e8f8
EX-Stras <stras3pp@gmail.com>
2026-02-20 22:40:15 -0300

    nada

--- Cap3/dimensoes&distancias/script.js
+++ Cap3/dimensoes&distancias/script.js
@@ -39,3 +39,48 @@ if(telap.matches){
 } else {
     console.log('telag')
 }
+
+console.log('EXERCÍCIO:')
+
+// Verifique a distância da primeira imagem
+// em relação ao topo da página
+
+const img1 = document.querySelector('img')
+
+console.log(img1.offsetTop)
+
+// Retorne a soma da largura de todas as imagens
+
+const imgs2 = document.querySelectorAll('img')
+
+let soma = 0;
+
+imgs2.forEach((item) => {
+   soma += item.clientWidth;
+})
+
+console.log(soma)
+
+
+// Verifique se os links da página possuem
+// o mínimo recomendado para telas utilizadas
+// com o dedo. (48px/48px de acordo com o google)
+
+const links1 = document.querySelectorAll('a')
+
+links1.forEach((item) => {
+    if (item.clientWidth < 48){
+        console.log('Não tem o necessário!!!')
+    } else {
+        console.log('Tudo certo.')
+    }
+})
+
+// Se o browser for menor que 720px,
+// adicione a classe menu-mobile ao menu
+
+const menu1 = document.querySelector('menu')
+
+if (window.innerWidth < 720){
+    menu1.classList.add('menu-mobile')
+}
