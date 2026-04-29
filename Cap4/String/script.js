a6d0955b22a79ae9694eac3a9a2326c0f40df11a
lucasmenezes <stras3pp@gmail.com>
2026-04-28 22:31:22 -0300

    Nada

--- Cap4/String/script.js
+++ Cap4/String/script.js
@@ -84,23 +84,122 @@ function animacao_scroll(){
 animacao_scroll()
 
 
+function Strings(){
+    const nome = "Lucas",
+        ano = new String(2018);
 
-const nome = "Lucas"
-const ano = new String(2018) 
+    console.log(nome[--nome.length])
+    console.log(ano)
 
-console.log(nome[--nome.length])
+    const nomeCompleto = nome.concat(ano,' ','hetero')
+    console.log(nomeCompleto)
 
-const nomeCompleto = nome.concat(ano,' ','hetero')
+    console.log(nome.includes('Lu'))
+    console.log(nome.startsWith('Lu'), nome.endsWith('as'))
 
-console.log(nome.includes('Lu'))
-console.log(nome.startsWith('Lu'), nome.endsWith('as'))
+    const exemplo = 'hetero '
 
-const exemplo = 'hetero'
+    console.log(exemplo.slice(-3, -2))
 
-console.log(exemplo.slice(-3, -2))
+    console.log(exemplo.indexOf('e'))
+    console.log(exemplo.lastIndexOf('e'))
 
-console.log(exemplo.indexOf('e'))
-console.log(exemplo.lastIndexOf('e'))
+    console.log(exemplo.padEnd(10, '.'))
+    console.log(exemplo.padStart(10, '.'))
 
-console.log(exemplo.padEnd(10, '.'))
-console.log(exemplo.padStart(10, '.'))
+    console.log(exemplo.repeat(5))
+
+    console.log(exemplo.replace('r','v'))
+
+    let listaItens = 'Camisas Bonés Calças Bermudas Vestidos Saias';
+    listaItens = listaItens.replace(/[ ]+/g, ', ');
+    console.log(listaItens)
+
+    console.log(exemplo.split('e'))
+
+    let nomes = ["Lucas","José","Gustavo"];
+    console.log(nomes.join(', '))
+
+    const cumprimento = '   ola   ';
+    console.log(
+        cumprimento.trimStart(),
+        cumprimento.trimEnd(),
+        cumprimento.trim()
+    )
+}
+Strings()
+
+// Utilizando o foreach na array abaixo,
+// some os valores de Taxa e os valores de Recebimento
+
+
+const transacoes1 = [
+  {
+    descricao: 'Taxa do Pão',
+    valor: 'R$ 39',
+  },
+  {
+    descricao: 'Taxa do Mercado',
+    valor: 'R$ 129',
+  },
+  {
+    descricao: 'Recebimento de Cliente',
+    valor: 'R$ 99',
+  },
+  {
+    descricao: 'Taxa do Banco',
+    valor: 'R$ 129',
+  },
+  {
+    descricao: 'Recebimento de Cliente',
+    valor: 'R$ 49',
+  },
+];
+
+let informacao = [];
+let x = 0;
+transacoes1.forEach((item) => {
+    if (item.descricao.includes('Taxa')){
+    informacao[x] = item.descricao.concat(': ', item.valor)
+    x++;
+    }
+})
+
+console.log(informacao)
+
+// Retorne uma array com a lista abaixo
+const transportes = 'Carro;Avião;Trem;Ônibus;Bicicleta';
+
+let transportes2 = [];
+transportes2 = transportes.split(';');
+console.log(transportes2)
+
+// Substitua todos os span's por a's
+let html = `    
+<ul>
+    <li><span>Sobre</span></li>
+    <li><span>Produtos</span></li>
+    <li><span>Contato</span></li>
+</ul>`;
+
+let html2 = [];
+html2 = html.split('span')
+html = html2.join('a')
+console.log(html)
+
+// Retorne o último caracter da frase
+const frase = 'Melhor do ano!';
+console.log(frase[frase.length - 1])
+
+// Retorne o total de taxas
+
+const transacoes2 = ['Taxa do Banco', '   TAXA DO PÃO', '  taxa do mercado', 'depósito Bancário', 'TARIFA especial'];
+let quantidadeDeTaxas = 0;
+
+transacoes2.forEach(item => {
+    if(item.toLowerCase().includes('taxa')){
+        quantidadeDeTaxas++;
+    }
+})
+
+console.log(quantidadeDeTaxas)
