111861da1f9e4c0a95d8281a47afcabd65a9abfa
lucasmenezes <stras3pp@gmail.com>
2026-05-03 22:02:40 -0300

    Nada

--- Cap4/Array/script.js
+++ Cap4/Array/script.js
@@ -154,3 +154,45 @@ function ArrayConstruct(){
     )
 }
 ArrayConstruct()
+
+console.log()
+console.log()
+console.log()
+
+let comidas = ['Pizza', 'Frango', 'Carne', 'Macarrão'];
+// Remova o primeiro valor de comidas e coloque em uma variável
+const comidaStart = comidas.shift()
+// Remova o último valor de comidas e coloque em uma variável
+const comidaLast = comidas.pop()
+// Adicione 'Arroz' ao final da array
+comidas.push('Arroz')
+// Adicione 'Peixe' e 'Batata' ao início da array
+comidas.unshift('Peixe','Batata')
+
+const estudantes = ['Marcio', 'Brenda', 'Joana', 'Kleber', 'Julia'];
+// Arrume os estudantes em ordem alfabética
+estudantes.sort()
+// Inverta a ordem dos estudantes
+estudantes.reverse()
+// Verifique se Joana faz parte dos estudantes
+console.log(estudantes.includes('Joana'))
+// Verifique se Juliana faz parte dos estudantes
+console.log(estudantes.includes('Juliana'))
+let html = `<section>
+              <div>Sobre</div>
+              <div>Produtos</div>
+              <div>Contato</div>
+            </section>`
+// Substitua section por ul e div com li,
+// utilizando split e join
+
+html = html.split('section').join('ul').split('div').join('li')
+
+const carros = ['Ford', 'Fiat', 'VW', 'Honda'];
+// Remova o último carro, mas antes de remover
+// salve a array original em outra variável
+
+const carrosCopia = carros.slice()
+
+carros.pop()
+
