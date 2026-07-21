570424eba30d6a54c0d33ca1fef7e3c8d704af5b
EX-Stras <stras3pp@gmail.com>
2026-07-21 19:00:42 -0300

    modal refatorado!

--- Cap11/RefatorarModal/js/Animacoes/modal.js
+++ Cap11/RefatorarModal/js/Animacoes/modal.js
@@ -21,22 +21,3 @@ export default class ModalP {
     this.selector3.addEventListener('click', (event) => this.toggleModal(event));
   }
 }
-
-// const modal = document.querySelector('section[data-modal=conteiner]');
-// const button = document.querySelector('header a[href^=https]');
-// const modalX = document.querySelector('section button[data-modal=fecharX]');
-//
-// function toogleModal(event) {
-// event.preventDefault();
-// modal.classList.toggle(modal.dataset.modal);
-// }
-//
-// function foraModal(event) {
-// if (event.target === modal) {
-// toogleModal(event);
-// }
-// }
-//
-// button.addEventListener('click', toogleModal);
-// modalX.addEventListener('click', toogleModal);
-// modal.addEventListener('click', foraModal);
